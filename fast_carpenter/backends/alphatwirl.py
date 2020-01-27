4277f96bbb98abc0826426cd11af04f2f5339952
kreczko <lkreczko@googlemail.com>
2020-01-27 15:56:48 +0000

    moved event_builder to backends.alphatwirl

--- fast_carpenter/backends/alphatwirl.py
+++ fast_carpenter/backends/alphatwirl.py
@@ -2,6 +2,100 @@
 Functions to run a job using alphatwirl
 """
 
+import uproot
+from atuproot.BEvents import BEvents
+from ..masked_tree import MaskedUprootTree
+from .. import dataspace
+
+class EventRanger():
+    def __init__(self):
+        self._owner = None
+
+    def set_owner(self, owner):
+        self._owner = owner
+
+    @property
+    def start_entry(self):
+        return (self._owner.start_block + self._owner.iblock) * self._owner.nevents_per_block
+
+    @property
+    def stop_entry(self):
+        i_block = min(self._owner.iblock + 1, self._owner.nblocks)
+        stop_entry = (self._owner.start_block + i_block) * self._owner.nevents_per_block
+        return min(self._owner.nevents_in_tree, stop_entry)
+
+    @property
+    def entries_in_block(self):
+        if self._owner and self._owner.iblock > -1:
+            return self.stop_entry - self.start_entry
+        return None
+
+
+class BEventsWrapped(BEvents):
+    def __init__(self, trees, *args, **kwargs):
+        ranges = EventRanger()
+        # TODO: this should be independent of "trees" - a simple contract between data format
+        # and BEventsWrapped should be enough
+        if not isinstance(trees, (dict)):
+            trees = {trees.name.decode('utf-8'): trees}
+        # trees = {name: MaskedUprootTree(tree, ranges) for name, tree in trees.items()}
+        ds = dataspace.group('input_trees', trees)
+        super(BEventsWrapped, self).__init__(ds, *args, **kwargs)
+        ranges.set_owner(self)
+
+    def _block_changed(self):
+        # TODO: Do we need to reset the mask/cache here?
+        # block changes should be injected --> self.tree.blockchange()
+        # self.tree.reset_mask()
+        # self.tree.reset_mask()
+        # self.tree['input_trees'].notify(actions=['reset_mask', 'reset_cache'])
+        pass
+
+    def __getitem__(self, i):
+        result = super(BEventsWrapped, self).__getitem__(self, i)
+        self._block_changed()
+        return result
+
+    def __iter__(self):
+        for value in super(BEventsWrapped, self).__iter__():
+            self._block_changed()
+            yield value
+        self._block_changed()
+
+
+class EventBuilder(object):
+    def __init__(self, config):
+        self.config = config
+
+    def __repr__(self):
+        return '{}({!r})'.format(
+            self.__class__.__name__,
+            self.config,
+        )
+
+    def __call__(self):
+        if len(self.config.inputPaths) != 1:
+            # TODO - support multiple inputPaths
+            raise AttributeError("Multiple inputPaths not yet supported")
+
+        # Try to open the tree - some machines have configured limitations
+        # which prevent memmaps from begin created. Use a fallback - the
+        # localsource option
+        try:
+            rootfile = uproot.open(self.config.inputPaths[0])
+            trees = { treeName: rootfile[treeName] for treeName in self.config.treeName}
+        except MemoryError:
+            rootfile = uproot.open(self.config.inputPaths[0],
+                                   localsource=uproot.FileSource.defaults)
+            trees = {treeName: rootfile[treeName] for treeName in self.config.treeName}
+
+        events = BEventsWrapped(trees,
+                                self.config.nevents_per_block,
+                                self.config.start_block,
+                                self.config.stop_block)
+        events.config = self.config
+        return events
+
 
 class DummyCollector():
     def collect(self, *args, **kwargs):
@@ -15,7 +109,6 @@ class AtuprootContext:
         self._orig_event_builder = atup.EventBuilder
         self._orig_build_parallel = atup.build_parallel
 
-        from ..event_builder import EventBuilder
         from atsge.build_parallel import build_parallel
         atup.EventBuilder = EventBuilder
         atup.build_parallel = build_parallel
