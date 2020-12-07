014cd87cb33d254b94a57e2f8617df934c716757
Luke Kreczko <lkreczko@googlemail.com>
2020-12-07 22:06:51 +0000

    switching to Python version 3.7-3.9

--- setup.py
+++ setup.py
@@ -21,9 +21,20 @@ def get_version():
     return _globals["__version__"]
 
 
-requirements = ['atuproot==0.1.13', 'atsge==0.2.1', 'atpbar==1.0.8', 'mantichora==0.9.7',
-                'alphatwirl==0.25.5', 'fast-flow>0.5.0', 'fast-curator', 'awkward',
-                'pandas>=1.1', 'numpy', 'numexpr', 'uproot>=3']
+requirements = [
+    'alphatwirl==0.25.5',
+    'atpbar==1.0.8',
+    'atsge==0.2.1',
+    'atuproot==0.1.13',
+    'awkward',
+    'fast-curator',
+    'fast-flow>0.5.0',
+    'mantichora==0.9.7',
+    'numpy',
+    'numexpr',
+    'uproot>=3',
+    'pandas>=1.1',
+]
 repositories = []
 
 setup_requirements = ['pytest-runner', ]
@@ -38,13 +49,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="F.A.S.T. package for summarizing ROOT TTrees",
     entry_points={
